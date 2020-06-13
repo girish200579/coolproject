3c71d4d25f9d9a6a0869e423c7edb46c31e20277
Girish Verma <girish.train@gmail.com>
2020-06-13 14:48:00 +0530

    added Jupiter at 3rd position

--- Hello.js
+++ Hello.js
@@ -1,6 +1,6 @@
 console.log('Hello Galaxy!');
 console.log('Hello world!');
-console.log('Hello world3!');
+console.log('Hello Jupiter!');
 console.log('Hello venus!');
 console.log('Hello Mars!');
 console.log('Hello Stars!');
